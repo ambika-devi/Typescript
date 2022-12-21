02992e45d21a1fb227ef2a4b2d904f0f20d47dc8
ambika-devi <ambikadevithota5@gmail.com>
2022-12-21 16:17:35 +0530

    creating apis using typescript in postman

--- app.js
+++ app.js
@@ -1,47 +1,12 @@
 "use strict";
-const n1 = document.getElementById('num1');
-const n2 = document.getElementById('num2');
-const buttonElement = document.querySelector('button');
-//creating arrays using generics
-const numResults = [];
-const textResults = [];
-function add(num1, num2) {
-    if (typeof num1 === 'number' && num2 === 'number')
-        return num1 + num2;
-    else {
-        if (typeof num1 === 'string' && num2 === 'string')
-            return num1 + '' + num2;
-    }
-    return +num1 + +num2;
-}
-// //usage of Objects can define it is object and also the keys and there types
-// function result(resultObj: Result) {
-//     console.log(`result ${resultObj.val} at ${resultObj.timeStamp}`);
-//   }
-function printResult(resultObj) {
-    console.log(resultObj.val);
-}
-buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', () => {
-    const num1 = n1.value;
-    const num2 = n2.value;
-    const result = add(+num1, +num2);
-    numResults.push(result);
-    const stringResults = add(num1, num2);
-    textResults.push(stringResults);
-    printResult({
-        val: result,
-        timeStamp: new Date()
-    });
-    console.log(numResults, textResults);
-});
-//cresting promises
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('it worked');
-    }), 1000;
-});
-promise.then(result => {
-    console.log(result.split('w'));
-}).catch(err => {
-    console.log(err);
-});
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+const todos_1 = __importDefault(require("./routes/todos"));
+app.use(todos_1.default);
+app.use(body_parser_1.default.json());
+app.listen(3000);
---
+++ routes/todos.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = (0, express_1.Router)();
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    res.status(201).json({ message: 'Added new todo', todo: newTodo, todos: todos });
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const todoid = req.params.todoId;
+    const todoindex = todos.findIndex((todoItem) => todoItem.id === todoid);
+    if (todoindex >= 0) {
+        todos[todoindex] = { id: todos[todoindex].id, text: req.body.text };
+        return res.status(200).json({ message: 'Updated', todos: todos });
+    }
+    res.status(400).json({ message: 'could not find todo for this id' });
+});
+router.delete('/todo/:todoId', (req, res, next) => {
+    let found = false;
+    todos = todos.filter((todoItem) => {
+        if (todoItem.id !== req.params.todoId) {
+            return true;
+        }
+        else {
+            found = true;
+            return false;
+        }
+    });
+    if (found) {
+        return res.status(200).json({ message: 'Deleted', todos: todos });
+    }
+    else
+        res.status(400).json({ message: 'could not find todo for this id,Unsuccful' });
+});
+exports.default = router;
